0c2e5baab1c1ab21f0307b159bec5224b4cb982e
Katie-Lui <katielui51@gmail.com>
2026-09-22 02:56:39 +0000

    Added the create_thumbnail function

--- .project/gather_metadata.sh
+++ .project/gather_metadata.sh
@@ -31,10 +31,21 @@ find_image() {
 }
 
 create_thumbnails() {
+
+  # in the directory the img is found in, create .thumbs directory if it doesnt exist yet
+  if [ ! -a .thumbs ] ; then
+    mkdir .thumbs
+  fi
+
+  # in the dir the img is found in, create .metadata if it doesnt exist yet
+  if [ ! -a .metadata ] ; then
+    mkdir .metadata
+  fi
+
   # get the width and height using identify and delimiting using spaces (cut),
   # getting the 3rd section of identify then delimiting again with the x, and storing the width and height into variables
-  Side_1=$(identify "$1"  | cut -d" " -f"3" | cut -d'x' -f"1")
-  Side_2=$(identify "$1"  | cut -d" " -f"3" | cut -d'x' -f"2")
+  Side_1=$(identify "$image"  | cut -d" " -f"3" | cut -d'x' -f"1")
+  Side_2=$(identify "$image"  | cut -d" " -f"3" | cut -d'x' -f"2")
 
   # determine which one (width or height) is longer, store it in long_side
   if [ $Side_1 -ge $Side_2 ] ; then
@@ -45,9 +56,28 @@ create_thumbnails() {
   fi
 
 
-  # check if long_side is smaller than 128, 
-  # elif smaller than 256, smaller than 512, or larger,
+  # check if long_side is greater than 128, 
+  # elif greater than 256, greater than 512,
   # and make thumbnails for those sizes
 
-  
+  cd .thumbs
+
+  # for this, we need to put the image file name into the $image var
+  if [ $long_side -ge 128 ] ; then
+    convert "$image" -resize 128x128 "$(echo $image | cut -d"." -f"1,2" --output-delimiter="-128.")"
+  fi
+  if [ $long_side -ge 256 ] ; then
+    convert "$image" -resize 256x256 "$(echo $image | cut -d"." -f"1,2" --output-delimiter="-256.")"
+  fi
+  if [ $long_side -ge 512 ] ; then
+    convert "$image" -resize 512x512 "$(echo $image | cut -d"." -f"1,2" --output-delimiter="-512.")"
+  fi
+
+
+  # going back to the directory the original img is in
+  cd ..
+
+  # creating the metadata file for the given image
+  identify -verbose $image > .metadata/"$image".txt
+
 }
