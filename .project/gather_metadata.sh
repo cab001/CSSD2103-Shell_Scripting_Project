4cf4c8642a2db474790a3f15a8954046f4d8113f
Katie-Lui <katielui51@gmail.com>
2026-09-22 00:42:23 +0000

    added function that changes the changeDir variable based on user input, and finished the part of the creating thumbnail function that tells us what side of the image is longer & stores it

--- .project/gather_metadata.sh
+++ .project/gather_metadata.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
 
+set_directory() {
+  # if no arguements given, current working directory is selected
+  if [ $# -eq 0 ] ; then
+    searchDir=./
+
+  # else if 1 is given, that will be the current working directory
+  elif [ $# -eq 1 ] ; then
+    searchDir="$1"
+  fi
+}
+
+
 is_directory() {
   if [[ ! -d "$1" ]]; then
     echo "$1 is not a directory.">&2
@@ -16,4 +28,26 @@ permission_check() {
 
 find_image() {
   find "$1" \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.tif" -o -iname "*.tiff" -o -iname "*.bmp" -o -iname "*.gif" \)
+}
+
+create_thumbnails() {
+  # get the width and height using identify and delimiting using spaces (cut),
+  # getting the 3rd section of identify then delimiting again with the x, and storing the width and height into variables
+  Side_1=$(identify "$1"  | cut -d" " -f"3" | cut -d'x' -f"1")
+  Side_2=$(identify "$1"  | cut -d" " -f"3" | cut -d'x' -f"2")
+
+  # determine which one (width or height) is longer, store it in long_side
+  if [ $Side_1 -ge $Side_2 ] ; then
+    long_side=$Side_1
+
+  else
+    long_side=$Side_2
+  fi
+
+
+  # check if long_side is smaller than 128, 
+  # elif smaller than 256, smaller than 512, or larger,
+  # and make thumbnails for those sizes
+
+  
 }
